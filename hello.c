c6a96689a9948069d862910c0c9eb58717ecfd29
Mohamed Ashraf <mohamedaashraf104@gmail.com>
2025-02-27 00:24:31 +0200

    Hello Github

---
+++ hello.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    char name[100];
+
+    printf("Enter your name: ");
+    scanf("%s", name);
+
+    printf("Congrat %s, You Learnt How To Use Github\n", name);
+    return 0;
+}
